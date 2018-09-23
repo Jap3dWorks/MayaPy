26c29e8737ccb25aa3331eafad091f03bade1521
Jap3dWorks <38395064+Jap3dWorks@users.noreply.github.com>
2018-09-23 18:03:32 +0200

    UV orient
    handle error

--- Tools/uvOrient.py
+++ Tools/uvOrient.py
@@ -1,14 +1,16 @@
 import Tools
 import maya.cmds as mc
 
+
 def uvOrientAndLookUp():
-	saveSelection = mc.ls (sl = True, type = 'float2')
-	shells = Tools.UvShellTools.uvShells()
-	mc.select (saveSelection, r = True)
-	Tools.UvShellTools.stackShells()
-	mc.select(clear = True)
-	for shell in shells:
-		Tools.UvShellTools.uvShellOrient (Shell = shell)
-		Tools.UvShellTools.uvShellUp (Shell = shell)
-		
+    saveSelection = mc.ls(sl=True, type='float2')
+    shells = Tools.UvShellTools.uvShells()
+    mc.select(saveSelection, r=True)
+    Tools.UvShellTools.stackShells()
+    mc.select(clear=True)
+    for shell in shells:
+        Tools.UvShellTools.uvShellOrient(Shell=shell)
+        Tools.UvShellTools.uvShellUp(Shell=shell)
+
+
 uvOrientAndLookUp()
