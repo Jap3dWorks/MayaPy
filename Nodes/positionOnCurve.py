1432336630b2cbeebddca03fbdf7f3d82215181d
Jap3dWorks <38395064+Jap3dWorks@users.noreply.github.com>
2018-10-02 23:04:41 +0200

    posiition on curve node
    posiition on curve node

--- Nodes/positionOnCurve.py
+++ Nodes/positionOnCurve.py
@@ -1,6 +1,7 @@
 import sys
 
 import maya.api.OpenMaya as OpenMaya
+import maya.api.OpenMayaUI as OpenMayaUI
 # documentation: https://help.autodesk.com/view/MAYAUL/2018/ENU/?guid=__files_GUID_AABB9BB4_816F_4C7C_A526_69B0568E1587_htm
 # documentation: https://help.autodesk.com/view/MAYAUL/2018/ENU/?guid=__files_GUID_767D6572_552D_4D94_90E2_AE626F276D37_htm
 # documentation: https://help.autodesk.com/view/MAYAUL/2018/ENU/?guid=__files_GUID_C5A68952_EB7D_41CC_ABF5_CC4C63293EED_htm
@@ -14,7 +15,7 @@ kPluginNodeName = 'positionOnCurve'
 kPluginNodeId = OpenMaya.MTypeId(0x010fff)
 
 # we use the maya proxy as base class for our node
-class positionOnCurve(OpenMaya.MPxNode):
+class positionOnCurve(OpenMayaUI.MPxLocatorNode):
     # define inputs
     inputCurveAttribute = OpenMaya.MObject()
     curvePositionAttribute = OpenMaya.MObject()
@@ -30,6 +31,13 @@ class positionOnCurve(OpenMaya.MPxNode):
     def initialize():
         # create attributes
         mfnAttr = OpenMaya.MFnNumericAttribute()
+
+        # Ramp attribute
+        rampAttrFn = OpenMaya.MRampAttribute()
+        rampAttribute = rampAttrFn.createCurveRamp('curveRamp', 'curveRamp')
+
+
+        positionOnCurve.addAttribute(rampAttribute)
 
         # positionAttr
         positionOnCurve.curvePositionAttribute = mfnAttr.create('Position', 'p', OpenMaya.MFnNumericData.kFloat, 0.5)
@@ -67,6 +75,8 @@ class positionOnCurve(OpenMaya.MPxNode):
         positionOnCurve.attributeAffects(positionOnCurve.inputCurveAttribute, positionOnCurve.outputMatrixTransformAttribute)
         positionOnCurve.attributeAffects(positionOnCurve.curvePositionAttribute, positionOnCurve.outputMatrixTransformAttribute)
 
+        # rampAttr
+        positionOnCurve.attributeAffects(rampAttribute, positionOnCurve.outputMatrixTransformAttribute)
 
     def __init__(self):
         super(positionOnCurve, self).__init__()
@@ -79,10 +89,10 @@ class positionOnCurve(OpenMaya.MPxNode):
 
         # obtain data handles for each attribute and values
         inputCurveHandle = pDataBlock.inputValue(positionOnCurve.inputCurveAttribute)  # get mDataHandle
-        inputCurve = inputCurveHandle.asNurbsCurve()  # mObject
+        inputCurve = inputCurveHandle.asNurbsCurveTransformed()  # mObject
 
         curvePositionHandle = pDataBlock.inputValue(positionOnCurve.curvePositionAttribute)  # get mDataHandle
-        curvePosition = curvePositionHandle.asFloat()  #float
+        curvePosition = curvePositionHandle.asFloat()  # float
 
         # obtain outputHandle
         outputMatrixTransformHandle = pDataBlock.outputValue(positionOnCurve.outputMatrixTransformAttribute)  # get mDataHandle
@@ -91,18 +101,17 @@ class positionOnCurve(OpenMaya.MPxNode):
         # create curves fn
         inputCurveMfn = OpenMaya.MFnNurbsCurve(inputCurve)
         # TODO: search dag node
-        inputCurveMfn.setObject(inputCurveMfn.getPath())  # set as dag path, to allow kworld transforms
         curveLenght = inputCurveMfn.length()
 
         pointPosition = curvePosition * curveLenght
         curveParam = inputCurveMfn.findParamFromLength(pointPosition)
         try:
-            curveNormal = inputCurveMfn.normal(curveParam, OpenMaya.MSpace.kWorld)
+            curveNormal = inputCurveMfn.normal(curveParam)
         except:
-            curveNormal = inputCurveMfn.normal(curveParam-0.001, OpenMaya.MSpace.kWorld) if not curvePosition == 0 else inputCurveMfn.normal(curveParam+0.001, OpenMaya.MSpace.kWorld)
-        curveTangent = inputCurveMfn.tangent(curveParam, OpenMaya.MSpace.kWorld)
+            curveNormal = inputCurveMfn.normal(curveParam-0.001) if not curvePosition == 0 else inputCurveMfn.normal(curveParam+0.001)
+        curveTangent = inputCurveMfn.tangent(curveParam)
         curveBinormal = curveTangent ^ curveNormal
-        curvePoint = inputCurveMfn.getPointAtParam(curveParam, OpenMaya.MSpace.kWorld)
+        curvePoint = inputCurveMfn.getPointAtParam(curveParam)
 
         # create transformMatrix
         mMatrix = OpenMaya.MFloatMatrix(((curveTangent.x, curveTangent.y, curveTangent.z, 0.0),
--- TESTFOLDER/Test.py
+++ TESTFOLDER/Test.py
@@ -1,8 +1,124 @@
-import logging
-logging.basicConfig()
-lHandler = logging.Handler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-lHandler.setFormatter(formatter)
-logger = logging.getLogger('Spline Distribute UI')
-logger.setLevel(logging.DEBUG)
-logger.addHandler(lHandler)
+import maya.api.OpenMaya as OpenMaya
+import maya.cmds as cmds
+
+def curveCreateEP():
+    mselList = OpenMaya.MGlobal.getActiveSelectionList()
+    
+    mFndagObject = OpenMaya.MFnDagNode()
+    dependNode = mFndagObject.create('transform', 'curve')
+    
+    # points
+    mPointArray = OpenMaya.MPointArray()
+    mPointArray.append(OpenMaya.MPoint(0,0,0))
+    mPointArray.append(OpenMaya.MPoint(10,0,0))
+    mPointArray.append(OpenMaya.MPoint(20,10,0))
+    mPointArray.append(OpenMaya.MPoint(20,20,0))
+    mPointArray.append(OpenMaya.MPoint(20,30,0))
+    
+    mFnCurve = OpenMaya.MFnNurbsCurve()
+    mFnCurve.createWithEditPoints(mPointArray, 7, 1, False, False, False, dependNode)
+ 
+    
+    # get area
+    area = mFnCurve.area(1.0)
+    print area
+
+
+
+def curveCreateCV(cv=((0,0,0), (5,0,0), (10,0,0), (20,10,5), (20,15,10), (20,20,15)), knots=((0.0), (0.0), (0.0), (3), (7), (10), (10), (10))):
+    """
+    Create a curve and snap an object to the center
+    Args:
+        cv: Curve vertex
+        knots: Knots
+    """
+    mFndagObject = OpenMaya.MFnDagNode()
+    dependNode = mFndagObject.create('transform', 'curve')
+    
+    # CV
+    mPointArray = OpenMaya.MPointArray(cv)
+    
+    # knots
+    KnotArray = OpenMaya.MDoubleArray(knots)
+    # create curve
+    mFnCurve = OpenMaya.MFnNurbsCurve()
+    mFnCurve.create(mPointArray, KnotArray, 3, 1, False, False, dependNode)
+
+    mfntransformCurve = OpenMaya.MFnTransform(dependNode)
+    mfntransformCurve.setTranslation(OpenMaya.MVector(15, 15, 15), 2)
+    mfntransformCurve.setRotation(OpenMaya.MEulerRotation(OpenMaya.MVector(15, 15, 15)), 1)
+
+    # if mfn is not set with dag path, cant do world transforms
+    curveDagpath = mfntransformCurve.getPath()
+    mFnCurve.setObject(curveDagpath)
+
+    print ('dag path: %s' % curveDagpath)
+
+    
+    # get area
+    area = mFnCurve.area(1.0)
+    print (area)
+
+    # get lenght
+    curveLenght = mFnCurve.length()
+    print (curveLenght)
+
+    middlePoint = mFnCurve.getPointAtParam(5.0, OpenMaya.MSpace.kWorld)
+    middleNormal = mFnCurve.normal(5.0, OpenMaya.MSpace.kWorld)
+    middleNormal.normalize()
+    middleTangent = mFnCurve.tangent(5.0, OpenMaya.MSpace.kWorld)
+    middleTangent.normalize()
+    middleBinormal = middleTangent ^ middleNormal
+    middleBinormal.normalize()
+
+    print(middleNormal, middleTangent, middleBinormal)
+
+    mselList = OpenMaya.MGlobal.getActiveSelectionList()
+    mDagPath = mselList.getDagPath(0)
+
+    transformation = OpenMaya.MMatrix(((middleTangent.x, middleTangent.y, middleTangent.z, 0.0),
+                                      (middleNormal.x, middleNormal.y, middleNormal.z, 0.0),
+                                      (middleBinormal.x, middleBinormal.y, middleBinormal.z, 0.0),
+                                      (middlePoint.x, middlePoint.y, middlePoint.z, 1)))
+
+    mfnTransform = OpenMaya.MFnTransform(mDagPath)
+    mfnTransform.setTransformation(OpenMaya.MTransformationMatrix(transformation))
+
+
+
+    """
+    # rotate vector a over vector b
+    quaternion = OpenMaya.MQuaternion(OpenMaya.MVector(1,0,0), middleTangent)
+
+    mfnTransform = OpenMaya.MFnTransform(mDagPath)
+    mfnTransform.setRotation(quaternion, 1)
+    mfnTransform.setTranslation(OpenMaya.MVector(middlePoint), 4)
+    """
+
+
+
+def getdestination(element, attribute):
+    mselList = OpenMaya.MSelectionList()
+    mselList.add(element)
+    meshObj = mselList.getDependNode(0)
+    mfnShape = OpenMaya.MFnMesh(meshObj)
+    mplug = mfnShape.findPlug(attribute, True)
+
+    print mplug.name()
+    print mplug.numChildren()
+    print mplug.numConnectedChildren()
+    print mplug.isConnected
+    print mplug.numConnectedElements()
+    print mplug.isElement
+    print mplug.numElements()
+
+    for i in range(mplug.evaluateNumElements()):
+        mchild = mplug.elementByPhysicalIndex(i)
+        print mchild.name()
+        print mchild.numConnectedChildren()
+        print mchild.isConnected
+        print mchild.connectedTo(True, True)[0].name()
+        print mchild.isElement
+
+
+# getdestination(element='polySurfaceShape3', attribute='instObjGroups')
