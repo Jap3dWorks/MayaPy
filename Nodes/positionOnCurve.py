3f6b257ed040e65db46101f007188a27aa07ff84
Jap3dWorks <38395064+Jap3dWorks@users.noreply.github.com>
2018-10-18 21:23:09 +0200

    PositionOnCurve node
    add basic twist control, dag node, fix a lot of bugs. better commented

--- Nodes/positionOnCurve.py
+++ Nodes/positionOnCurve.py
@@ -4,97 +4,100 @@ import maya.OpenMaya as OpenMaya
 import maya.OpenMayaUI as OpenMayaUI
 import maya.OpenMayaMPx as OpenMayaMPx
 
+import math
 import pymel.core as pm
 import maya.cmds as cmds
 import maya.mel as mel
+
+import logging
+logging.basicConfig()
+logger = logging.getLogger('Position on curve:')
+logger.setLevel(logging.INFO)
 
 # documentation: https://help.autodesk.com/view/MAYAUL/2018/ENU/?guid=__files_GUID_AABB9BB4_816F_4C7C_A526_69B0568E1587_htm
 # documentation: https://help.autodesk.com/view/MAYAUL/2018/ENU/?guid=__files_GUID_767D6572_552D_4D94_90E2_AE626F276D37_htm
 # documentation: https://help.autodesk.com/view/MAYAUL/2018/ENU/?guid=__files_GUID_C5A68952_EB7D_41CC_ABF5_CC4C63293EED_htm
 # documentation: https://stackoverflow.com/questions/50365958/maya-mpxnode-multiple-outputs
 
-# TODO: AE TEMPLATE
+# TODO: Study object creation and new plugs inside Node, this works with callbacks
+# TODO: add twist
 
 kPluginVersion = "0.0"
 kPluginNodeName = 'positionOnCurve'
 # identifier of the node
-kPluginNodeId = OpenMaya.MTypeId(0x010fff)
+kPluginNodeId = OpenMaya.MTypeId(0x87059)
 kPluginNodeClassify = 'utility/general'
 kPluginNodeNameManip = 'positionOnCurveManip'
-kPluginNodeNameManipId = OpenMaya.MTypeId(0x010ff9)
+kPluginNodeNameManipId = OpenMaya.MTypeId(0x87060)
 kPluginNodeNameCommand = 'positionOnCurveCommand'
+kPluginNodeClassify = 'utility/general'  # not in use
 
-# we use the maya proxy as base class for our node
+# we use the maya locator proxy as base class for our node
 class positionOnCurve(OpenMayaMPx.MPxLocatorNode):
     # define inputs
     inputCurveAttribute = OpenMaya.MObject()
     curvePositionAttribute = OpenMaya.MObject()
     rampScaleAttr = OpenMaya.MObject()
-    manipAttr = OpenMaya.MObject()
+    rampTwistAttr = OpenMaya.MObject()
+
     # define outputs
     outputMatrixTransformAttribute = OpenMaya.MObject()
 
     @classmethod
     def creator(cls):
-        # Explanation: node creator for classic python API
+        # node creator for classic python API
         return OpenMayaMPx.asMPxPtr(cls())
 
     @staticmethod
-    def initialize():
+    def nodeInitializer():
         # associate node with its manipulator
         OpenMayaMPx.MPxManipContainer.addToManipConnectTable(kPluginNodeId)
 
         # To make things more readable
         node = positionOnCurve
 
-        # ManipAttr
-        mfnAttr = OpenMaya.MFnNumericAttribute()
-        # documentation: https://help.autodesk.com/view/MAYAUL/2018/ENU/?guid=__cpp_ref_class_m_fn_distance_manip_html
-        # Explanation: data type corresponding to distanceManip is a double
-        node.manipAttr = mfnAttr.create('ManipValue', 'MV', OpenMaya.MFnNumericData.kDouble)
-        mfnAttr.setHidden(False)
-        node.addAttribute(node.manipAttr)
+        # Ramp attributes
+        rampAttrFn = OpenMaya.MRampAttribute()
+        node.rampScaleAttr = rampAttrFn.createCurveRamp('rampScaleAttr', 'rampScaleAttr')
+        node.addAttribute(node.rampScaleAttr)
+        # rampTwistAttr
+        rampAttrFn = OpenMaya.MRampAttribute()
+        node.rampTwistAttr = rampAttrFn.createCurveRamp('rampTwistAttr', 'rampTwistAttr')
+        node.addAttribute(node.rampTwistAttr)
 
         # create attributes
         mfnAttr = OpenMaya.MFnNumericAttribute()
 
-        # Ramp attribute
-        rampAttrFn = OpenMaya.MRampAttribute()
-        node.rampScaleAttr = rampAttrFn.createCurveRamp('curveRamp', 'curveRamp')
-
-        node.addAttribute(node.rampScaleAttr)
-
         # positionAttr
-        node.curvePositionAttribute = mfnAttr.create('Position', 'p', OpenMaya.MFnNumericData.kDouble, 0.5)
+        node.curvePositionAttribute = mfnAttr.create('PositionCurve', 'PositionCurve', OpenMaya.MFnNumericData.kDouble, 0.5)
         mfnAttr.setKeyable(True)
         mfnAttr.setStorable(True)
-        mfnAttr.setReadable(False)
         mfnAttr.setWritable(True)
         mfnAttr.setMin(0.0)
         mfnAttr.setMax(1.0)
+        # add attributes just after create attribute
+        node.addAttribute(node.curvePositionAttribute)
 
         # transformationAttr
         mfnAttr = OpenMaya.MFnMatrixAttribute()
-        node.outputMatrixTransformAttribute = mfnAttr.create('Transformation', 't', 0)  # 1 double precision
+        node.outputMatrixTransformAttribute = mfnAttr.create('Transformation', 'Transformation', OpenMaya.MFnNumericData.kDouble)  # 1 double precision
         mfnAttr.setKeyable(False)
         mfnAttr.setStorable(True)
         mfnAttr.setReadable(True)
         mfnAttr.setWritable(False)
         mfnAttr.setHidden(False)
+        node.addAttribute(node.outputMatrixTransformAttribute)
 
         # curveAttr
+        # explanation: curve input OpenMaya.MFnData.kNurbsCurve. no OpenMaya.MFn.kNurbsCurve
         mfnAttr = OpenMaya.MFnTypedAttribute()
-        node.inputCurveAttribute = mfnAttr.create('Curve', 'c', 16)
+        node.inputCurveAttribute = mfnAttr.create('Curve', 'Curve', OpenMaya.MFnData.kNurbsCurve)
         mfnAttr.setKeyable(False)
         mfnAttr.setStorable(False)
         mfnAttr.setReadable(False)
         mfnAttr.setWritable(True)
         mfnAttr.setHidden(False)
-
-        # attach attributes
-        node.addAttribute(node.curvePositionAttribute)
         node.addAttribute(node.inputCurveAttribute)
-        node.addAttribute(node.outputMatrixTransformAttribute)
 
         # define attributes relations to update
         node.attributeAffects(node.inputCurveAttribute, node.outputMatrixTransformAttribute)
@@ -102,9 +105,22 @@ class positionOnCurve(OpenMayaMPx.MPxLocatorNode):
 
         # rampAttr
         node.attributeAffects(node.rampScaleAttr, node.outputMatrixTransformAttribute)
+        node.attributeAffects(node.rampTwistAttr, node.outputMatrixTransformAttribute)
+
+        return True
 
     def __init__(self):
-        super(positionOnCurve, self).__init__()
+        # return to super
+        OpenMayaMPx.MPxLocatorNode.__init__(self)
+
+    def postConstructor(self):
+        OpenMaya.MFnDependencyNode(self.thisMObject()).setName('positionOnCurveShape#')
+        # define a callback, here we can for example duplicate objects and connect
+        # (for that porpouse we need array attr).
+        self.callbackID = OpenMaya.MNodeMessage.addAttributeChangedCallback(self.thisMObject(), self.attrChangeCallback)
+
+    def attrChangeCallback(self, *args):
+        logger.debug('CallBack ## attr changed ##')
 
     def isBounded(self):
         return True
@@ -115,32 +131,73 @@ class positionOnCurve(OpenMayaMPx.MPxLocatorNode):
     # plug represents output attribute
     # dataBlock all values
     def compute(self, pPlug, pDataBlock):
-        # TODO: add twist
+        logger.debug('--Compute Method--')
         if pPlug != self.outputMatrixTransformAttribute:  # Review: self.
             return None
 
-        # obtain data handles for each attribute and values
+        #  data handles for each attribute and values
         inputCurveHandle = pDataBlock.inputValue(positionOnCurve.inputCurveAttribute)  # get mDataHandle
         inputCurve = inputCurveHandle.asNurbsCurveTransformed()  # mObject
 
         curvePositionHandle = pDataBlock.inputValue(positionOnCurve.curvePositionAttribute)  # get mDataHandle
         curvePosition = curvePositionHandle.asDouble()  # asDouble
 
-        # obtain outputHandle
+        # rampScaleAttr, don't use dataBlock. MRampAttribute instead
+        rampScaleHandle = OpenMaya.MRampAttribute(OpenMaya.MPlug(self.thisMObject(), positionOnCurve.rampScaleAttr))
+        rampTwistHandle = OpenMaya.MRampAttribute(OpenMaya.MPlug(self.thisMObject(), positionOnCurve.rampTwistAttr))
+
+        # convert data to C++ types
+        # rampScaleValue
+        util = OpenMaya.MScriptUtil()
+        util.createFromDouble(0.0)
+        valuePtr = util.asFloatPtr()
+        rampScaleHandle.getValueAtPosition(curvePosition, valuePtr)
+
+        # read value from memory
+        rampScaleValue = util.getFloat(valuePtr)
+
+        #TwistScaleValue
+        util = OpenMaya.MScriptUtil()
+        util.createFromDouble(0.0)
+        valuePtr = util.asFloatPtr()
+        rampTwistHandle.getValueAtPosition(curvePosition, valuePtr)
+
+        # read value from memory
+        rampTwistValue = util.getFloat(valuePtr)
+
+        # outputHandle
         outputMatrixTransformHandle = pDataBlock.outputValue(positionOnCurve.outputMatrixTransformAttribute)  # get mDataHandle
 
         # compute
         # create curves fn
         inputCurveMfn = OpenMaya.MFnNurbsCurve(inputCurve)
-        # TODO: search dag node
         curveLenght = inputCurveMfn.length()
-
         pointPosition = curvePosition * curveLenght
         curveParam = inputCurveMfn.findParamFromLength(pointPosition)
-        curveNormal = inputCurveMfn.normal(max(curveParam - 0.001, 0.001))
 
+        # get Vectors
+        curveNormal = inputCurveMfn.normal(max(curveParam - 0.001, 0.001))
         curveTangent = inputCurveMfn.tangent(curveParam)
         curveBinormal = curveTangent ^ curveNormal
+
+        # rampTwist
+        rampTwistValue = (rampTwistValue - 0.5) * math.pi * 2  # possible add magnitude
+        util = OpenMaya.MScriptUtil()
+        util.createFromDouble(rampTwistValue)
+        quaternion = OpenMaya.MQuaternion(rampTwistValue, curveTangent)
+        curveNormal = curveNormal.rotateBy(quaternion)
+        curveBinormal = curveBinormal.rotateBy(quaternion)
+
+        # normalize vectors
+        curveNormal.normalize()
+        curveTangent.normalize()
+        curveBinormal.normalize()
+
+        # apply rampScale
+        curveNormal *= rampScaleValue * 4  # possible rang attribute
+        curveTangent *= rampScaleValue * 4
+        curveBinormal *= rampScaleValue * 4
+
         curvePoint = OpenMaya.MPoint()
         inputCurveMfn.getPointAtParam(curveParam, curvePoint)
 
@@ -164,7 +221,6 @@ def loadAETemplateCallback(nodeName):
 
 # documentation: http://help.autodesk.com/cloudhelp/2017/CHS/Maya-Tech-Docs/Commands/editorTemplate.html
 class AEpositionOnCurveTemplate(pm.ui.AETemplate):
-    print('class AE')
     def addControll(self, control, label=None, **kwargs):
         pm.ui.AETemplate.addControl(self, control, label=label, **kwargs)
 
@@ -174,43 +230,54 @@ class AEpositionOnCurveTemplate(pm.ui.AETemplate):
     def __init__(self, nodeName):
         super(AEpositionOnCurveTemplate, self).__init__(nodeName)
         self.thisNode = None
-        print('init AE')
         node = pm.PyNode(nodeName)
-        print node.type()
         if node.type() == kPluginNodeName:
-            print('Goood AE')
 
             for attr in pm.listAttr(nodeName):
                 self.suppress(attr)
-                print ('suppress %s.%s' % (nodeName, attr))
+                logger.debug('suppress %s.%s' % (nodeName, attr))
 
-            self.callCustom(lambda: self.showTitle(), lambda: None)
+            self.callCustom(self.showTitle, lambda: None)
 
             self.beginScrollLayout()
             self.beginLayout('General', collapse=False)
 
-            annotation = 'something'
+            annotation = 'testing annotation'
             # controlls
-            self.addControll('ManipValue', label='Manip Value', annotation=annotation)
+
+            self.addControll('PositionCurve', label='PositionCurve', annotation=annotation)
+
             self.endLayout()
 
+            # custom button
             # ramp control
             self.beginLayout('RampControl', collapse=True)
-            mel.eval('AEaddRampControl("' + nodeName + '.curveRamp");')
+            mel.eval('AEaddRampControl("%s.rampScaleAttr");' % nodeName)
+            mel.eval('AEaddRampControl("%s.rampTwistAttr");' % nodeName)
             self.endLayout()
+            # twistRampAttr
 
-            self.beginLayout('General2', collapse=False)
-            self.addControll('Position', label='Position', annotation=annotation)
 
-            self.addControll('Transformation', label='Transformation', annotation=annotation)
-
-            self.addControll('Curve', label='curve', annotation=annotation)
+            # explanation: AE add button
+            # call custom: newFunc, replaceFunc, *attrs
+            self.callCustom(self.newButton, self.buttonUpdate, 'PositionCurve')
 
             self.endLayout()
 
             self.endScrollLayout()
 
+    def newButton(self, nodeName):
+        pm.rowLayout(numberOfColumns=3, adjustableColumn=1, columnWidth3=(80, 100, 100))
+        self.showNewManip = pm.button(label='Edit Manip', command=lambda *args: self.buttonShowManip(nodeName))
 
+    def buttonUpdate(self, attr):
+        nodeName = pm.PyNode(attr).nodeName()
+        self.showNewManip.setCommand(lambda *args: self.buttonShowManip(nodeName))
+
+    def buttonShowManip(self, nodeName):
+        pm.select(cl=True)
+        pm.select(nodeName)
+        pm.runtime.ShowManipulators()
 
     def showTitle(self):
         pm.text('Position On Curve v' + kPluginVersion, font='boldLabelFont')
@@ -234,7 +301,7 @@ class positionOnCurveCommand(OpenMayaMPx.MPxCommand):
 
         # undo queue reversed, undo first the las action
         for m in reversed(self.mUndo):
-            m.undoIt()  # <- MDGModifier
+            m.undoIt()  # <- MDagModifier
             """
             Undoes all of the operations that have been given to this modifier.
             It is only valid to call this method after the doIt method has been called.
@@ -279,11 +346,21 @@ class positionOnCurveCommand(OpenMayaMPx.MPxCommand):
         transformFn = OpenMaya.MFnDagNode(shapeDagPath)
 
         # create positionOnCurve Node
-        mdgModifier = OpenMaya.MDGModifier()
-        self.mUndo.append(mdgModifier)
-        positionOnCurveNode = mdgModifier.createNode(kPluginNodeId)
-        mdgModifier.doIt()
-        positionOnCurveNodeFn = OpenMaya.MFnDependencyNode(positionOnCurveNode)
+        mdagModifier = OpenMaya.MDagModifier()
+        self.mUndo.append(mdagModifier)
+        posOnCurveTransform = mdagModifier.createNode(kPluginNodeId)  # kPluginNodeId
+        mdagModifier.doIt()
+        posOnCurveTransformFn = OpenMaya.MFnDagNode(posOnCurveTransform)
+        posOnCurveTransformFn.setName('positionOnCurve#')
+
+        # create a transform node for PositionOnCurve
+        # posOnCurveTransformFn.setName('positionOnCurveLocator#')
+
+        # get locator shape
+        nodeShapeDagPath = OpenMaya.MDagPath()
+        posOnCurveTransformFn.getPath(nodeShapeDagPath)
+        nodeShapeDagPath.extendToShape()
+        nodeShapeFN = OpenMaya.MFnDagNode(nodeShapeDagPath)
 
         # need to create a decomposeMatrix node
         mdgModifier = OpenMaya.MDGModifier()
@@ -292,21 +369,36 @@ class positionOnCurveCommand(OpenMayaMPx.MPxCommand):
         mdgModifier.doIt()
         decomposeMatrixFn = OpenMaya.MFnDependencyNode(decomposeMatrixHandle)
 
+        # todo: function with this
+        # default values Ramp Attribute, if not the plug give errors on start
+        defaultPosition = OpenMaya.MFloatArray(1, 0.0)
+        defaultValues = OpenMaya.MFloatArray(1, 0.25)
+        defaultInterpolations = OpenMaya.MIntArray(1, 3)
+        plug = nodeShapeFN.findPlug(positionOnCurve.rampScaleAttr)
+        ramp = OpenMaya.MRampAttribute(plug)
+        ramp.addEntries(defaultPosition, defaultValues, defaultInterpolations)
+        # rampTwistAttr
+        defaultValues = OpenMaya.MFloatArray(1, 0.5)
+        plug = nodeShapeFN.findPlug(positionOnCurve.rampTwistAttr)
+        ramp = OpenMaya.MRampAttribute(plug)
+        ramp.addEntries(defaultPosition, defaultValues, defaultInterpolations)
+
         # connect Nodes
         mdgModifier = OpenMaya.MDGModifier()
         self.mUndo.append(mdgModifier)
-        # curve -> positionOnCurve
-        mdgModifier.connect(curvePlug, positionOnCurveNodeFn.findPlug(positionOnCurve.inputCurveAttribute, False))
+        # curve -> positionOnCurve Node
+        mdgModifier.connect(curvePlug, nodeShapeFN.findPlug(positionOnCurve.inputCurveAttribute))
         # positionOnCurve -> decomposeMatrix
-        mdgModifier.connect(positionOnCurveNodeFn.findPlug(positionOnCurve.outputMatrixTransformAttribute), decomposeMatrixFn.findPlug('inputMatrix', False))
+        mdgModifier.connect(nodeShapeFN.findPlug(positionOnCurve.outputMatrixTransformAttribute), decomposeMatrixFn.findPlug('inputMatrix', False))
         # decomposeMatrix -> mesh transform
         mdgModifier.connect(decomposeMatrixFn.findPlug('outputTranslate', False), transformFn.findPlug('translate', False))
         mdgModifier.connect(decomposeMatrixFn.findPlug('outputRotate', False), transformFn.findPlug('rotate', False))
+        mdgModifier.connect(decomposeMatrixFn.findPlug('outputScale', False), transformFn.findPlug('scale', False))
         mdgModifier.doIt()
 
         # select positionOnCurve Node
         cmds.select(clear=True)
-        cmds.select(positionOnCurveNodeFn.name())
+        cmds.select(posOnCurveTransformFn.name())
         # Open AE
         mel.eval('openAEWindow')
 
@@ -331,12 +423,13 @@ class positionOnCurveManip(OpenMayaMPx.MPxManipContainer):
         """
         list = OpenMaya.MSelectionList()
         OpenMaya.MGlobal.getActiveSelectionList(list)
+        self.distManip = None
 
         # no more than one object selected
         if list.length() != 1:
             return None
 
-        self.distManip = self.addDistanceManip('positionManip', 'position')
+        self.distManip = self.addDistanceManip('positionManip', 'PositionCurve')
 
     def connectToDependNode(self, node):
         self.nodeFn.setObject(node)
@@ -373,16 +466,13 @@ class positionOnCurveManip(OpenMayaMPx.MPxManipContainer):
         OpenMayaMPx.MPxManipContainer.connectToDependNode(self, node)
 
 
-
-
-
-
-
-
-
 def initializePlugin(plugin):
     pluginFn = OpenMayaMPx.MFnPlugin(plugin, 'Jap3D', kPluginVersion)
     try:
+        # register node, don't miss OpenMayaMPx.MPxNode.kLocatorNode!!!
+        pluginFn.registerNode(kPluginNodeName, kPluginNodeId, positionOnCurve.creator, positionOnCurve.nodeInitializer,
+                              OpenMayaMPx.MPxNode.kLocatorNode)
+
         # register command, no initialize need for commands
         pluginFn.registerCommand(kPluginNodeNameCommand, positionOnCurveCommand.cmdCreator)
 
@@ -392,9 +482,6 @@ def initializePlugin(plugin):
         # register manip
         pluginFn.registerNode(kPluginNodeNameManip, kPluginNodeNameManipId, positionOnCurveManip.nodeCreator,
                               positionOnCurveManip.nodeInitializer, OpenMayaMPx.MPxNode.kManipContainer)
-
-        # register node
-        pluginFn.registerNode(kPluginNodeName, kPluginNodeId, positionOnCurve.creator, positionOnCurve.initialize)
 
     except:
         sys.stderr.write('Failed to register node: ' + kPluginNodeName)
@@ -406,6 +493,8 @@ def uninitializePlugin(plugin):
         pluginFn.deregisterNode(kPluginNodeId)
         pluginFn.deregisterCommand(kPluginNodeNameCommand)
         pluginFn.deregisterNode(kPluginNodeNameManipId)
+
+        pm.callbacks(removeCallback=loadAETemplateCallback, hook='AETemplateCustomContent', owner=kPluginNodeName)
 
     except:
         sys.stderr.write('Failed to deregister node: ' + kPluginNodeName)
