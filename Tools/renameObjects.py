f3393f4ddebf6b0abf53791d6344ffbbdb72200a
Jap3dWorks <38395064+Jap3dWorks@users.noreply.github.com>
2018-11-02 11:40:19 +0100

    Rename Objects name convention Tool
    Rename Objects name convention Tool

--- Tools/renameObjects.py
+++ Tools/renameObjects.py
@@ -10,74 +10,71 @@ def renameObjects(objects=None, name='name', zone=None,  side=None, identifier=N
         identifier: extra identifier info
         side: 'left' or 'right', if this arg is not specified it will be auto with z axis in front
     """
-
     selection = objects if objects else pm.ls(sl=True, dag=True)
-    print('object list: %s' % selection)
-    # TODO: diferent side, diferent numeration, with a dictionary?
-    # p.e: numLoc ={pm.nodetype:[type,{'left':1, 'right':1, None:1}]    } -> numLoc[side] += 1
-    typesObject = {}
+    # TODO: maybe dag is to dangerous. should be i.getShape() not i.getTransform()
+    print selection
 
-    for i in ('locator', 'aimConstraint', 'pointConstraint', 'mesh', 'ikHandle', 'ctr'):
-        sideDictionaty = {}
+    typesObject = {}
+    pynodeTypes = (pm.nodetypes.Locator, 'locator',
+                   pm.nodetypes.AimConstraint, 'aimConstraint',
+                   pm.nodetypes.PointConstraint, 'pointConstraint',
+                   pm.nodetypes.Mesh, 'mesh',
+                   pm.nodetypes.IkHandle, 'ikHandle',
+                   pm.nodetypes.NurbsCurve, 'ctr',
+                   pm.nodetypes.Transform, 'grp',
+                   pm.nodetypes.ParentConstraint, 'parentConstraint',
+                   pm.nodetypes.Joint, 'jnt')
+
+    for i in range(0, len(pynodeTypes), 2):
+        sideDic = {}
+        pynodeType = [pynodeTypes[i+1]]
         for n in ('left', 'right', None):
-            sideDictionaty[n] = 1
-        typesObject[i] = sideDictionaty
-    
+            sideDic[n] = 1
+
+        pynodeType.append(sideDic)
+        typesObject[pynodeTypes[i]] = pynodeType
+
     for i in selection:
         rename = name
         typeNode = None
         num = None
 
         # if no side, auto side
-        # nurbs
         if not side:
             try:
                 translation = i.getTranslation(space='world')
             except:
                 translation = i.getTransform().getTranslation(space='world')
 
-            print('%s translation: %s' % (str(i), translation))
-            sideTemp = 'left' if translation[0] > 0 else 'right' if translation[0] < 0 else None
             # x = 0, no side identifier
+            sideTemp = 'left' if translation[0] > 0 else 'right' if translation[0] < 0 else None
 
         else:
             sideTemp = side
 
-        if isinstance(i, pm.nodetypes.Locator):
-            typeNode = 'locator'
-            num = typesObject[typeNode][sideTemp]
-            typesObject[typeNode][sideTemp] += 1
-        elif isinstance(i, pm.nodetypes.AimConstraint):
-            typeNode = 'aimConstraint'
-            num = typesObject[typeNode][sideTemp]
-            typesObject[typeNode][sideTemp] += 1
-        elif isinstance(i, pm.nodetypes.PointConstraint):
-            typeNode = 'pointConstraint'
-            num = typesObject[typeNode][sideTemp]
-            typesObject[typeNode][sideTemp] += 1
-        elif isinstance(i, pm.nodetypes.Mesh):
-            typeNode = 'mesh'
-            num = typesObject[typeNode][sideTemp]
-            typesObject[typeNode][sideTemp] += 1
-        elif isinstance(i, pm.nodetypes.IkHandle):
-            typeNode = 'ikHandle'
-            num = typesObject[typeNode][sideTemp]
-            typesObject[typeNode][sideTemp] += 1
-        elif isinstance(i, pm.nodetypes.NurbsCurve):
-            typeNode = 'ctr'
-            num = typesObject[typeNode][sideTemp]
-            typesObject[typeNode][sideTemp] += 1
+        if type(i) in typesObject:
+            if isinstance(i, pm.nodetypes.Transform) and i.getShape():
+                continue
+
+            typeNode = typesObject[type(i)][0]
+            num = typesObject[type(i)][1][sideTemp]
+            print('pre plus num %s: %s' % (typesObject[type(i)][0], typesObject[type(i)][1][sideTemp]))
+            typesObject[type(i)][1][sideTemp] += 1
+            print('post plus num: %s' % typesObject[type(i)][1][sideTemp])
+
         else:
-            # if is transform and do not has shape, it is a grp
-            if isinstance(i, pm.nodetypes.Transform) and not i.getShape():
-                typeNode = 'grp'
+            continue
 
         for a in (zone, sideTemp, identifier, num, typeNode):
             if a:
                 rename += '_%s' % a
+
+        # if node has transform, we should rename the transform node
         try:
             i = i.getTransform()
         except:
             pass
 
         i.rename(rename)
+
+    print typesObject
