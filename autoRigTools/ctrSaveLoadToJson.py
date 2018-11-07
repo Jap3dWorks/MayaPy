fd6f2f8f36e152014b9740af7be0113ae40cf94b
Jap3dWorks <38395064+Jap3dWorks@users.noreply.github.com>
2018-11-07 21:20:26 +0100

    save controls to json
    save controls to json

--- autoRigTools/ctrSaveLoadToJson.py
+++ autoRigTools/ctrSaveLoadToJson.py
@@ -25,6 +25,8 @@ def ctrSaveJson(typeController, name, path):
 
     # get selection only use the first element
     selection = pm.ls(sl=True)[0]
+    if not selection:
+        raise ValueError('Select one nurbs curve')
     
     # if file not exist, create
     if not os.path.exists(controllerFile):
@@ -35,16 +37,33 @@ def ctrSaveJson(typeController, name, path):
     with open(controllerFile, 'r') as f:
         # json to dictionary
         controllerDict = json.load(f)
-    
+
+    # check if we have a previous controller os the same type
+    if typeController in controllerDict.keys():
+        confirmDialog = pm.confirmDialog(m=('%s is already stored, do you want to replace?' % typeController).capitalize(), button=['Replace','Cancel'])
+
+        if confirmDialog != 'Replace':
+            logger.info(('%s %s controller not saved' % (name, typeController)).capitalize())
+            return
+
     # list where save list attributes
     controllerAttr = []
-    # points must be list of floats
-    curveShape = selection.getShape()
-    points = curveShape.getCVs()
-    controllerAttr.append([(i.x, i.y, i.z) for i in points])
-    controllerAttr.append(curveShape.getKnots())
-    controllerAttr.append(curveShape.attr('degree').get())
-    controllerAttr.append(curveShape.attr('form').get())
+
+    curveShapes = selection.listRelatives(s=True, c=True)
+    for curveShape in curveShapes:
+        print curveShape
+        if not isinstance(curveShape, pm.nodetypes.NurbsCurve):
+            raise ValueError('Controller must be a nurbs curve')
+        shapeAttr = []
+        points = curveShape.getCVs()
+        # points must be list of floats
+        shapeAttr.append([(i.x, i.y, i.z) for i in points])
+        shapeAttr.append(curveShape.getKnots())
+        shapeAttr.append(curveShape.attr('degree').get())
+        shapeAttr.append(curveShape.attr('form').get())
+
+        # one list per shape in the controller
+        controllerAttr.append(shapeAttr)
     
     # save controller to dictionary
     controllerDict[typeController] = controllerAttr
@@ -56,8 +75,9 @@ def ctrSaveJson(typeController, name, path):
     logger.info('%s %s controller saved at: %s' % (name, typeController, controllerFile))
 
 
-def ctrLoadJson(typeController, name, path, SFactor=1):
+def ctrLoadJson(typeController, name, path, SFactor=1, ColorIndex = 4):
     """
+    TODO: color attributes here too
     Load saved controllers from json
     Args:
         typeController: controller name
@@ -75,13 +95,23 @@ def ctrLoadJson(typeController, name, path, SFactor=1):
 
     # list with controller parameters
     ctrParameters = controllerDict[typeController]
-    curveFn = OpenMaya.MFnNurbsCurve()
-    # create controller
-    form = curveFn.kOpen if ctrParameters[3] == 0 else curveFn.kPeriodic
-    # multiplyFactor
-    CVPoints = [(i[0]*SFactor, i[1]*SFactor, i[2]*SFactor) for i in ctrParameters[0]]
-    # create curve
-    curveFn.create(CVPoints, ctrParameters[1], ctrParameters[2], form, False, True)
-    newControllerDagPath = OpenMaya.MDagPath.getAPathTo(curveFn.object())
-    
-    return OpenMaya.MDagPath.getAPathTo(newControllerDagPath.transform()).fullPathName()
+    transform = OpenMaya.MObject()
+    for n, ctrParam in enumerate(ctrParameters):
+        curveFn = OpenMaya.MFnNurbsCurve()
+        # create controller
+        form = curveFn.kOpen if ctrParam[3] == 0 else curveFn.kPeriodic
+        # multiplyFactor
+        CVPoints = [(i[0]*SFactor, i[1]*SFactor, i[2]*SFactor) for i in ctrParam[0]]
+        # create curve
+        curveFn.create(CVPoints, ctrParam[1], ctrParam[2], form, False, True,
+                       transform if isinstance(transform, OpenMaya.MObject) else transform.node())
+        # set color
+        enhableColorsPlug = curveFn.findPlug('overrideEnabled', False)
+        enhableColorsPlug.setBool(True)
+        colorPlug = curveFn.findPlug('overrideColor', False)
+        colorPlug.setInt(ColorIndex)
+        newControllerDagPath = OpenMaya.MDagPath.getAPathTo(curveFn.object())
+        if n == 0:
+            transform = OpenMaya.MDagPath.getAPathTo(newControllerDagPath.transform())
+
+    return transform.fullPathName()
