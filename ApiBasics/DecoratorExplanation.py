2f499c1729f5f4ed0993f37f1a2dd50262bfa8e7
Jap3dWorks <38395064+Jap3dWorks@users.noreply.github.com>
2018-11-18 16:18:50 +0100

    api utilities
    add new decorator example

--- ApiBasics/DecoratorExplanation.py
+++ ApiBasics/DecoratorExplanation.py
@@ -56,7 +56,7 @@ def eggs(func):
     return wrapper
 
 def eggsShit(func):
-    print ('this is shit')
+    print ('this is egg shit')
     @wraps(func)  # decorator factory
     def wrapper(*args, **kwargs):  # create tuple and dictionaries from values
         print ('this is wrapper')
@@ -75,3 +75,57 @@ def goodbye(name = 'mary'):
 goodbye()
 
 # print(goodbye.__name__)
+
+#######################
+#DECORATORS IN A CLASS#
+#######################
+from maya import cmds
+import time
+class cubeCreator(object):
+    # decorator class
+    # other examples in autoRig
+    class decoratorClass(object):
+        def __init__(self, key):
+            self.key = key
+
+        def __call__(self, func):
+            def inner(*args, **kwargs):
+                print ('hello from', self.key)
+
+                check = None
+
+                if 'left' in args or 'right' in args:
+                    check = 'left'
+
+                if 'side' in kwargs:
+                    check = True
+
+                print ('check %s' % check)
+
+                timeTrack = time.time()
+                result = func(*args, **kwargs)
+
+                print ('time: %s' % (time.time() - timeTrack))
+                return result
+
+            return inner
+
+    def __init__(self, x, y, z):
+        super(cubeCreator, self).__init__()
+
+        self.x = x
+        self.y = y
+        self.z = z
+
+    @decoratorClass('keyArg')
+    def create_cube(self, name, side, world='earth'):
+        cmds.polyCube(h=self.x, d=self.y, w=self.z, name=name + side)
+
+    @decoratorClass('sphere')
+    def create_sphere(self, name, world='earth'):
+        cmds.polySphere(r=self.x, name=name)
+
+
+cubecreatorvar = cubeCreator(10, 10, 10)
+cubecreatorvar.create_cube('francesca', side='right', world='earth')
+cubecreatorvar.create_sphere('manoly')
